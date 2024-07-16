c802eabd110537f2b37272ad58dac93204e4583b
Rodrigo Gouveia <rgestevao@gmail.com>
2024-07-16 20:56:40 -0300

    feat: initializing service

---
+++ api/courseservice/src/test/java/br/com/api/educa/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.api.educa.courseservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
