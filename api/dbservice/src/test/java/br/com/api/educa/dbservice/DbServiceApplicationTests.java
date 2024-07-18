0e8adf0f0aae968540239c3aa0d14583cd407a63
Rodrigo Gouveia <rgestevao@gmail.com>
2024-07-18 19:48:41 -0300

    feat: initializing service

---
+++ api/dbservice/src/test/java/br/com/api/educa/dbservice/DbServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.api.educa.dbservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
